8f1967887cdf1feb055ccb89fb8ac460d93df409
Eugene Chan <me@eugenechantk.me>
2022-04-04 15:39:09 +0100

    simplify connection to databse

--- query.py
+++ query.py
@@ -2,13 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from query import Office
-
 # establish the connection to the database created from create.py
 engine = create_engine('sqlite:///realestate.db', echo=True)
 Base = declarative_base(engine)
 
-# start the session to insert data into the database
+# start the session to query data into the database
 Session = sessionmaker(bind=engine)
 session = Session()
-
